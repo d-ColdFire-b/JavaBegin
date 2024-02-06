c7ccbd8d507dc4a1ac8e6af4f4b89ff4f43fc531
Maxi Zhiltsov <zhilcov.maksim@gmail.com>
2024-02-07 00:32:09 +0400

    Save/Load File functionality.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,113 @@
 import entity.Person;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
 
-        Person java_gay = new Person(69);
+
+
+        String saveFileDirPath = "saves";
+        File saveFileDir = new File(saveFileDirPath);
+
+        Person person = new Person(1);
+
+
+        if (!saveFileDir.exists()) {
+            saveFileDir.mkdir();
+        }
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            String answer;
+            while (true) {
+                System.out.println("Load data from file? (Yes / No)");
+                answer = scanner.nextLine();
+                if (answer.equalsIgnoreCase("YES") || answer.equalsIgnoreCase("NO")) {
+                    break;
+                }
+                System.out.println("Unreadable answer, try again");
+            }
+            String fileName;
+//          Load from file
+            if (answer.equalsIgnoreCase("YES")) {
+
+                fileName = isFileNameOk(scanner);
+
+                File saveFile = new File(saveFileDirPath + "/" + fileName + ".txt");
+
+                person.printDetails(person.loadFrom(saveFile));
+
+
+//          Do NOT load from file
+            } else {
+
+                fileName = isFileNameOk(scanner);
+
+                File saveFile = new File(saveFileDirPath + "/" + fileName + ".txt");
+
+                person.saveTo(saveFile, getInfo(scanner));
+
+
+            }
+
+        }
+
 
     }
+
+
+    public static String isFileNameOk(Scanner scanner) {
+
+        String fileName;
+        while (true) {
+            System.out.println("Enter file name (W/out extension):");
+            fileName = scanner.nextLine();
+            if (!fileName.trim().isEmpty()) {
+                break;
+            }
+            System.out.println("Empty or White space-only name entered, try again!");
+        }
+        return fileName;
+    }
+
+    public static Person getInfo (Scanner scanner){
+
+        boolean loop;
+        String toCheck;
+        while (true){
+            loop = false;
+            System.out.println("Enter person's ID");
+            toCheck = scanner.nextLine();
+            if (toCheck.trim().isEmpty()){
+                System.out.println("Empty ID, try again");
+                continue;
+            }
+            for (int i = 0; i < toCheck.trim().toCharArray().length ; i++) {
+                if (!Character.isDigit(toCheck.trim().charAt(i))) {
+                    System.out.println("It's not digit you bastard!");
+                    loop = true;
+                }
+            }
+            if (!loop) {
+                break;
+            }
+        }
+
+
+        Person person = new Person(Integer.parseInt(toCheck));
+
+        System.out.println("Enter Name:");
+        person.setName(scanner.nextLine());
+        System.out.println("Enter Surname:");
+        person.setSurname(scanner.nextLine());
+        System.out.println("Enter Gender:");
+        person.setGender(scanner.nextLine());
+
+
+        return person;
+    }
+
 }
--- src/entity/Person.java
+++ src/entity/Person.java
@@ -1,26 +1,79 @@
 package entity;
 
+import javax.sound.midi.Soundbank;
+import java.io.*;
+import java.util.Scanner;
+
 public class Person {
 
-    int id,age;
-    String gender;
+    int id;
 
-    public Person(int id){
+    public void setId(int id) {
         this.id = id;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setGender(String gender) {
-        if (gender == "Male" || gender == "Female"){
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
+    String gender, name, surname;
+
+    public Person(int id) {
+        this.id = id;
+    }
+
+
+    public void setGender(String gender) {
+        if (gender.equals("Male") || gender.equals("Female")) {
             this.gender = gender;
 
-        }
-        else {
+        } else {
             this.gender = "Retard";
         }
     }
+
+    public static void saveTo(File file, Person person) throws IOException {
+        try (FileOutputStream stream = new FileOutputStream(file)) {
+            try (PrintWriter writer = new PrintWriter(stream)) {
+                writer.println(person.id);
+                writer.println(person.name);
+                writer.println(person.surname);
+                writer.println(person.gender);
+            }
+
+
+        }
+    }
+
+
+    public static Person loadFrom(File file) throws IOException {
+
+        try (FileInputStream stream = new FileInputStream(file)) {
+            try (Scanner scanner = new Scanner(stream)) {
+                Person person = new Person(scanner.nextInt());
+                scanner.nextLine();
+                person.setName(scanner.nextLine());
+                person.setSurname(scanner.nextLine());
+                person.setGender(scanner.nextLine());
+                return person;
+            }
+        }
+    }
+
+
+    public void printDetails(Person person) {
+
+        System.out.println("Id = " + person.id);
+        System.out.println("Name = " + person.name);
+        System.out.println("Surname = " + person.surname);
+        System.out.println("Gender = " + person.gender);
+
+
+    }
+
+
 }
